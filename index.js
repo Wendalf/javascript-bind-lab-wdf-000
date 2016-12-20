10d3e36b4de73a4e703d5f929a02bc1a779f7b0c
Wendy Xu <guangw.xu@gmail.com>
2016-12-20 18:24:29 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-const app = "I don't do much.";
+var digitalClock = {
+  time: Math.floor(Date.now()/1000),
+  startTicking: function() {
+    // setInterval(function() {this.time += 1;}.bind(this), 1000)
+    setInterval(() => this.time ++, 1000);
+  }
+}
+
+function censor(word, string) {
+  let regexify = new RegExp(word, 'gi');
+  return string.replace(regexify, "BLEEP");
+}
+
+const violenceCensor = censor.bind(null, 'violence');
+
+const drugsCensor = censor.bind(null, 'drugs');
+
